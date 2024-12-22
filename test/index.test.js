e9163cd3af4eee7473f5c25b9554a65310832ad7
Satishkumar Dhule <sdhule@salesforce.com>
2024-12-22 07:30:21 +0530

    feat: initial setup

---
+++ test/index.test.js
@@ -0,0 +1,8 @@
+// filepath: /Users/sdhule/personal/test/index.test.js
+const assert = require('assert');
+
+describe('Sample Test', () => {
+  it('should return true', () => {
+    assert.strictEqual(true, true);
+  });
+});
